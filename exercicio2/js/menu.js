24c64f0ad56f4555593e59258f6371ca427d1f09
@virmerson <virmerson@htcursos.com>
2018-03-03 16:19:42 -0400

    Menu Burger Navdraw

---
+++ exercicio2/js/menu.js
@@ -0,0 +1,8 @@
+function abrirMenu(){
+    document.getElementById("menu").classList.add("menu_aberto")
+}
+
+function fecharMenu(){
+   
+    document.getElementById("menu").classList.remove("menu_aberto")
+}
