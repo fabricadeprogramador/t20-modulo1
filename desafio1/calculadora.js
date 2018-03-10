205d09b7854a90ed85f327ebe53a4f5588720556
@virmerson <virmerson@htcursos.com>
2018-03-10 17:48:15 -0400

    Classe Calculadora

---
+++ desafio1/calculadora.js
@@ -0,0 +1,68 @@
+class Calculadora{
+
+    somar(){
+        //1) Ler dados convertidos em numeros dos inputs 
+        var n1 =  parseFloat(document.getElementById("n1").value);
+        var n2 = parseFloat(document.getElementById("n2").value);
+        
+        //2) Realizar a soma
+        var r = n1 + n2;
+
+        //3) Exibir o resultado
+        alert(r);
+    }
+
+     subtrair(){
+        //1) Ler dados convertidos em numeros dos inputs 
+        var n1 =  parseFloat(document.getElementById("n1").value);
+        var n2 = parseFloat(document.getElementById("n2").value);
+        
+        //2) Realizar a sub
+        var r = n1 - n2;
+
+        //3) Exibir o resultado
+        alert(r);
+    }
+
+    dividir(){
+        //1) Ler dados convertidos em numeros dos inputs 
+        var n1 =  parseFloat(document.getElementById("n1").value);
+        var n2 = parseFloat(document.getElementById("n2").value);
+        
+        //2) Realizar a div
+        var r = n1 / n2;
+
+        //3) Exibir o resultado
+        alert(r);
+
+    }
+
+    multiplicar(){
+        //1) Ler dados convertidos em numeros dos inputs 
+        var n1 =  parseFloat(document.getElementById("n1").value);
+        var n2 = parseFloat(document.getElementById("n2").value);
+        
+        //2) Realizar a mult
+        var r = n1 * n2;
+
+        //3) Exibir o resultado
+        alert(r);
+
+    }
+     porcentagem(){
+        //1) Ler dados convertidos em numeros dos inputs 
+        var n1 =  parseFloat(document.getElementById("n1").value);
+        var n2 = parseFloat(document.getElementById("n2").value);
+        
+        //2) Realizar a mult
+        var r = n1 * n2/100;
+
+        //3) Exibir o resultado
+        alert(n2+ "%  de " + n1 +" é " + r);
+
+    }
+
+}
+
+//Instanciar Objeto
+var calculadora =  new Calculadora();
