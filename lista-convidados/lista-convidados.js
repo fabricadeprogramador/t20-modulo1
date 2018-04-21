51c3433df2a7a78ec99853dbdf302eae5d741899
@virmerson <virmerson@htcursos.com>
2018-04-21 15:29:15 -0400

    Lista Div com Exclusão

--- lista-convidados/lista-convidados.js
+++ lista-convidados/lista-convidados.js
@@ -4,6 +4,7 @@ class ListaConvidados{
 
    constructor(){
        this.qtd = 0;
+       this.id = 0;
    }
     
     adicionar(){
@@ -11,19 +12,24 @@ class ListaConvidados{
         //1) Ler o input
          let nome = document.getElementById("inputConvidado").value;
         //2) inclusao na lista
-         
-        //2.1) Criar os elementos dos DOM
+         //2.1) Criar os elementos dos DOM
          let lista = document.getElementById("lista");
         
          let div = document.createElement("div");  // <div></div>
          div.classList.add("item-lista") // <div class="item-lista"></div>
-        
+         div.id= "conv-"+this.id; 
+         
          let span = document.createElement("span");//<span></span>
          span.innerText=nome;//<span>Jão da Silva</span>
 
          let img = document.createElement("img");//<img />
          img.src="img/delete.svg"; //<img src="img/delete.svg"/>
 
+         //<img onclick="lista.excluir('conv-0')">
+         img.setAttribute("onclick",  "lista.excluir('"  +  div.id + "')" )   ;  
+
+        // <img onclick="???">
+   
          //2.2) Aninhar as tags
         div.appendChild(span);  //<div class="item-lista"> <span>Jão da Silva</span></div>
         div.appendChild(img); //<div class="item-lista"> <span>Jão da Silva</span> <img src="img/delete.svg"> </div>
@@ -34,11 +40,18 @@ class ListaConvidados{
          //3)Contador
          
          this.qtd =this.qtd + 1;
-
+         this.id= this.id + 1;
          //4)Inserir quantidade no label
          let labelQtd = document.getElementById("labelQtd");
         labelQtd.innerText = this.qtd;
 
+    }
+
+    excluir(id){
+
+       let  conv = document.getElementById(id);
+       let lis = document.getElementById("lista");
+       lis.removeChild(conv);
     }
 
 
