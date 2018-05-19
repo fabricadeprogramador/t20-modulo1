ffed1f66858a686fdf0fa5492e2c29398d228e69
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2018-05-19 15:13:57 -0400

    lista de numeros

--- loops-arrays/lista-numeros.js
+++ loops-arrays/lista-numeros.js
@@ -6,9 +6,19 @@ class ListaNumeros{
         let n = parseInt(document.getElementById("n").value);
         //exibir numeros de 1 a N
 
-   
-        document.getElementById("lista").innerText= "1";
+        let el = document.getElementById("lista");
+        
+        let texto = el.innerText;
 
+        texto= 1;
+
+        for (let v = 2  ;   v<=n  ; v=v+1){
+
+         texto= texto+","+v;
+
+        }
+    
+        el.innerText = texto;
     }
 
 }
