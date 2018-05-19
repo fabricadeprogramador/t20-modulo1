1bd291f0f9e155ee4129ac142f3a2f00ca2b098c
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2018-05-19 14:44:35 -0400

    Iniciando loops

---
+++ loops-arrays/lista-numeros.js
@@ -0,0 +1,16 @@
+class ListaNumeros{
+
+
+    imprimirNumeros(){
+        // ler o numero digitado
+        let n = parseInt(document.getElementById("n").value);
+        //exibir numeros de 1 a N
+
+   
+        document.getElementById("lista").innerText= "1";
+
+    }
+
+}
+
+var listaNumeros =  new ListaNumeros();
